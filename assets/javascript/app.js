a34449c8451f1e706406a4eb4054dcabbd05c517
Calley O'Neil <calleyoneil@Calleys-MacBook-Pro.local>
2017-03-16 18:27:45 -0500

    for real this time

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+// trivia questions and answers: https://docs.google.com/document/d/1uSRyqfCXcpJpNpuzCX2-dwizAlfnxl1_rvLsHgh5FzA/edit?usp=sharing
+
+$(document).ready(function() {
+
+	// setting up the varriables
+	// tiriva questions with the answers and false answers
+	var triviaQandA = [ 
+		{question: "What is the only entirely non-Muggle village in Great Britain?" , answer: "Hogsmeade", incorrect: "London", "Liverpool", "Godric's Hollow" },
+		{question: "A witch or wizard who can transform into an animal at will is known as what?", answer: "Animagus" , incorrect: "Halfblood", "Ravenclaw", "Grubby-Plank"},
+		{question: "What two countries’ teams played in the Quidditch World Cup Harry attended before his fourth year at Hogwarts?", answer: "Ireland" "Bulgaria", incorrect: "Spain", "France"},
+
+
+
+		}
+
+	];
+// 
+
+// function to populate the div with the question, and buttons with answers .append the buttons?
+
+
+// clear out the guess
+	
+
+}
